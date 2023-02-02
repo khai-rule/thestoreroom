603fa17261394b86346adaf9fa49aad31ed15c5a
Khairul <Qai-rule@hotmail.com>
2023-02-02 13:09:36 +0800

    user name will appear at the top of the page when scroll

--- src/components/ProfileHead.tsx
+++ src/components/ProfileHead.tsx
@@ -1,16 +1,35 @@
 import { ProfileHeadProps } from "../utilities/interface";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
 import { CreatorsContext } from "../utilities/context";
+import { useEffect, useState, useRef, useContext } from "react";
 
 const ProfileHead: React.FC<ProfileHeadProps> = ({ matchingCreator }) => {
-	const userEmail = matchingCreator?.creator.email;
 	const navigate = useNavigate();
 	const { loggedInCreatorContentful } = useContext(CreatorsContext);
 
-	// const creator = creators.find(
-	// 	(creator: Creator) => _.lowerCase(creator?.creator?.email) === userEmail
-	// );
+	const [isSticky, setIsSticky] = useState(false);
+	const h1Ref = useRef<HTMLHeadingElement>(null);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (!h1Ref.current) {
+				return;
+			}
+			if (window.pageYOffset >= h1Ref.current.offsetTop) {
+				setIsSticky(true);
+			} else {
+				setIsSticky(false);
+			}
+		};
+
+		console.log(isSticky);
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	const loggedInCreatorArtistName =
 		loggedInCreatorContentful?.creator?.artistName;
 
@@ -34,36 +53,47 @@ const ProfileHead: React.FC<ProfileHeadProps> = ({ matchingCreator }) => {
 	const instagramLink = `https://www.instagram.com/${instagram}/?hl=en`;
 
 	return (
-		<>
-			<div className="text-center flex flex-col items-center mt-16">
-				{loggedInCreatorArtistName === artistName ? (
-					<button onClick={() => navigate("/account/edit")}>
-						Edit Profile
-					</button>
-				) : (
-					<> </>
-				)}
-				<h1>{name}</h1>
-				<h4>{title}</h4>
-				<p className="w-3/6 my-4">{bio}</p>
-				<div>
-					<a
-						className="mx-4 text-secondary font-semibold"
-						href={website}
-						target="_blank"
-					>
-						{formatWebsite ? formatWebsite : ""}
-					</a>
-					<a
-						className="mx-4 text-secondary font-semibold"
-						href={instagramLink}
-						target="_blank"
-					>
-						{instagram ? `@${instagram}` : ""}
-					</a>
-				</div>
+		<div className="text-center flex flex-col items-center mt-16">
+			{loggedInCreatorArtistName === artistName ? (
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+					onClick={() => navigate("/account/edit")}
+				>
+					Edit Profile
+				</button>
+			) : (
+				<></>
+			)}
+
+			<h1>{name}</h1>
+			<h4>{title}</h4>
+			<p className="w-3/6 my-4">{bio}</p>
+			<div className="flex" ref={h1Ref}>
+				<a
+					className="mx-4 text-secondary font-semibold"
+					href={website}
+					target="_blank"
+				>
+					{formatWebsite ? formatWebsite : ""}
+				</a>
+				<a
+					className="mx-4 text-secondary font-semibold"
+					href={instagramLink}
+					target="_blank"
+				>
+					{instagram ? `@${instagram}` : ""}
+				</a>
 			</div>
-		</>
+
+			<h3
+				className={`fixed top-6 items-center text-secondary z-10 transition-opacity duration-300 ease-in-out opacity-0 hover:cursor-pointer ${
+					isSticky ? "opacity-100" : "hover:cursor-default"
+				}`}
+				onClick={() => window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+			>
+				{name}
+			</h3>
+		</div>
 	);
 };
 
